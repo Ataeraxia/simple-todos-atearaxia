12b4e5d354cb8bc6203838be2ac8ebd499ccd9c5
Ataeraxia <you@example.com>
2017-12-05 15:46:13 -0500

    End lesson 7. Where has this been all my life.

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -7,15 +7,32 @@ class TodosListCtrl {
     constructor($scope) {
         $scope.viewModel(this);
 
+        this.hideCompleted = true;
+
         this.helpers({
             tasks() {
-                return Tasks.find({}, {
+                const selector = {};
+
+                if(this.getReactively("hideCompleted")) {
+                    selector.checked = {
+                        $ne: true
+                    };
+                }
+
+                return Tasks.find(selector, {
                     sort: {
                         // I did the opposite of -1 because I actually wanted to sort my tasks for the oldest at the top. I left the sorting in entirely so that I could see how to do it later.
                         createdAt: 1
                         // If you're reading this: How would I sort the tasks so that the checked ones would be at the bottom? I tried checked: -1 and checked: 1.
                     }
                 });
+            },
+            incompleteCount() {
+                return Tasks.find({
+                    checked: {
+                        $ne: true
+                    }
+                }).count();
             }
         })
 

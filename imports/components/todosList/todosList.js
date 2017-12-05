0960adf3e84ab302467835539e7cb4b019649b67
Ataeraxia <you@example.com>
2017-12-05 15:22:57 -0500

    End of lesson 5.

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -31,6 +31,18 @@ class TodosListCtrl {
 
         this.newTask = "";
     }
+
+    setChecked(task) {
+        Tasks.update(task._id, {
+            $set: {
+                checked: !task.checked
+            },
+        });
+    }
+
+    removeTask(task) {
+        Tasks.remove(task._id);
+    }
 }
 
 export default angular.module("todosList", [
